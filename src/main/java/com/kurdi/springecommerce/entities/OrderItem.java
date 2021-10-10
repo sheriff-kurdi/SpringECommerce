1054a15d8d0322dcbd227bc9444b70f51c101c8a
sheriff-kurdi <sheriff.kurdi@gmail.com>
2021-10-10 16:57:20 +0200

    one to many with one to many annotation

---
+++ src/main/java/com/kurdi/springecommerce/entities/OrderItem.java
@@ -0,0 +1,28 @@
+package com.kurdi.springecommerce.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id;
+    private String name;
+    private double price;
+    private String productId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_ID", nullable = false)
+    protected Order order;
+}
