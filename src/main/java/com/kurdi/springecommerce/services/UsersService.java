52ed8d3d0d10cd08ea237094936afbd9e03bcb6b
sheriff-kurdi <sheriff.kurdi@gmail.com>
2021-10-13 16:59:35 +0200

    user service transactional

--- src/main/java/com/kurdi/springecommerce/services/UsersService.java
+++ src/main/java/com/kurdi/springecommerce/services/UsersService.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class UsersService {
     @Autowired
     PasswordEncoder passwordEncoder;
