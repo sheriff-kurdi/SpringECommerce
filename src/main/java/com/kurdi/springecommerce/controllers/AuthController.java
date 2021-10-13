a31ecb00f31e0e20997d6ac3a6247e7e76d2d7ed
sheriff-kurdi <sheriff.kurdi@gmail.com>
2021-10-13 16:14:28 +0200

    refactor user service

--- src/main/java/com/kurdi/springecommerce/controllers/AuthController.java
+++ src/main/java/com/kurdi/springecommerce/controllers/AuthController.java
@@ -1,10 +1,12 @@
 package com.kurdi.springecommerce.controllers;
 
+import com.kurdi.springecommerce.domain.entities.CartsAggregate.Cart;
 import com.kurdi.springecommerce.domain.entities.UsersAggregate.AppUser;
 import com.kurdi.springecommerce.domain.entities.UsersAggregate.Authority;
-import com.kurdi.springecommerce.repositories.UserRepository;
+import com.kurdi.springecommerce.repositories.UsersRepository;
 import com.kurdi.springecommerce.security.Permissions;
 import com.kurdi.springecommerce.security.Roles;
+import com.kurdi.springecommerce.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,39 +18,12 @@ import java.util.List;
 
 @RestController
 public class AuthController {
+
     @Autowired
-    PasswordEncoder passwordEncoder;
-    @Autowired
-    UserRepository userRepository;
+    UsersService usersService;
 
     @GetMapping("/register")
-    public ResponseEntity<String> register()
-    {
-        List<Authority> authorities = new ArrayList<>();
-        AppUser user = AppUser.builder()
-                .username("sheriff")
-                .password(passwordEncoder.encode("123"))
-                .authorities(authorities)
-                .build();
-
-        authorities.add(Authority
-                .builder()
-                .appUser(user)
-                .name(Roles.ADMIN.getRole())
-                .build());
-
-        authorities.add(Authority
-                .builder()
-                .appUser(user)
-                .name(Permissions.EmployeeRead.getPermission())
-                .build());
-
-        if(userRepository.findUserByUsername(user.getUsername()).isEmpty())
-        {
-            userRepository.save(user);
-        }
-
-
-        return ResponseEntity.ok(user.getUsername());
+    public ResponseEntity<AppUser> register() {
+        return ResponseEntity.ok(usersService.register());
     }
 }
---
+++ src/main/java/com/kurdi/springecommerce/services/UsersService.java
@@ -0,0 +1,52 @@
+package com.kurdi.springecommerce.services;
+
+import com.kurdi.springecommerce.domain.entities.CartsAggregate.Cart;
+import com.kurdi.springecommerce.domain.entities.UsersAggregate.AppUser;
+import com.kurdi.springecommerce.domain.entities.UsersAggregate.Authority;
+import com.kurdi.springecommerce.repositories.UsersRepository;
+import com.kurdi.springecommerce.security.Permissions;
+import com.kurdi.springecommerce.security.Roles;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UsersService {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    UsersRepository userRepository;
+
+    public AppUser register() {
+        List<Authority> authorities = new ArrayList<>();
+        AppUser user = AppUser.builder()
+                .username("sheriff")
+                .password(passwordEncoder.encode("123"))
+                .authorities(authorities)
+                .build();
+
+        authorities.add(Authority
+                .builder()
+                .appUser(user)
+                .name(Roles.ADMIN.getRole())
+                .build());
+
+        authorities.add(Authority
+                .builder()
+                .appUser(user)
+                .name(Permissions.EmployeeRead.getPermission())
+                .build());
+
+        if (userRepository.findUserByUsername(user.getUsername()).isEmpty()) {
+            userRepository.save(user);
+            user.setCart(new Cart(user));
+            userRepository.save(user);
+
+        }
+
+        return user;
+    }
+}
