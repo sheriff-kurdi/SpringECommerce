28776e20494908f3d4db4d8d88d460ec760d094e
sheriff-kurdi <sheriff.kurdi@gmail.com>
2022-01-01 15:13:32 +0200

    Thymeleaf engine setup

--- src/main/java/com/kurdi/springecommerce/controllers/HomeController.java
+++ src/main/java/com/kurdi/springecommerce/controllers/HomeController.java
@@ -1,15 +1,16 @@
 package com.kurdi.springecommerce.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String guest()
     {
-        return "welcome guest";
+        return "home/index";
     }
 
     @GetMapping("/user")
