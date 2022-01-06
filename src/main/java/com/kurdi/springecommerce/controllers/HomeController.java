dc9899884e3d2de578afd7648529da30615938c8
sheriff-kurdi <sheriff.kurdi@gmail.com>
2022-01-06 15:59:38 +0200

    argon dashboard

--- src/main/java/com/kurdi/springecommerce/controllers/HomeController.java
+++ src/main/java/com/kurdi/springecommerce/controllers/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
     @GetMapping("/")
     public String guest()
     {
-        return "shared/index";
+        return "products/index";
     }
 
     @GetMapping("/user")
